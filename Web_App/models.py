2a1c112525850da42fb04e64335874922f637510
aleixBY23 <43808251+aleixBY23@users.noreply.github.com>
2023-02-20 17:15:26 +0100

    Base de dades creada

--- Web_App/models.py
+++ Web_App/models.py
@@ -1,3 +1,27 @@
 from django.db import models
 
+
 # Create your models here.
+class Wallet(models.Model):
+    balance = models.DecimalField(decimal_places=8, max_digits=12)
+    #Foreign key
+    #idUser = models.ForeignKey(User, on_delete=models.CASCADE)
+
+
+class User(models.Model):
+    name = models.TextField(null=True)
+    surname = models.TextField(null=True)
+    email = models.TextField()
+    username = models.TextField(null=True)
+    password = models.TextField()
+    avatar = models.TextField(null=True)
+    enabled = models.BooleanField(default=True)
+    id_wallet = models.ForeignKey(Wallet, on_delete=models.CASCADE)
+
+class Server(models.Model):
+    game = models.TextField()
+    version = models.DecimalField(decimal_places=2, max_digits=8)
+    cores = models.DecimalField(decimal_places=2, max_digits=8)
+    ram = models.DecimalField(decimal_places=2, max_digits=8)
+    storage = models.DecimalField(decimal_places=2, max_digits=8)
+    id_user = models.ForeignKey(User, on_delete=models.CASCADE)
--- web/settings.py
+++ web/settings.py
@@ -37,6 +37,7 @@ INSTALLED_APPS = [
     'django.contrib.sessions',
     'django.contrib.messages',
     'django.contrib.staticfiles',
+    'Web_App.apps.WebAppConfig',
 ]
 
 MIDDLEWARE = [
@@ -54,7 +55,8 @@ ROOT_URLCONF = 'web.urls'
 TEMPLATES = [
     {
         'BACKEND': 'django.template.backends.django.DjangoTemplates',
-        'DIRS': [],
+        'DIRS': [BASE_DIR / 'templates']
+        ,
         'APP_DIRS': True,
         'OPTIONS': {
             'context_processors': [
