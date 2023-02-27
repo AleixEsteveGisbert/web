b82ec663d79231bdca61d9009cfbdf4190a66fe4
aleixBY23 <43808251+aleixBY23@users.noreply.github.com>
2023-02-27 15:43:38 +0100

    Update models.py

--- Web_App/models.py
+++ Web_App/models.py
@@ -4,6 +4,8 @@ from django.db import models
 # Create your models here.
 class Wallet(models.Model):
     balance = models.DecimalField(decimal_places=8, max_digits=12)
+    created_at = models.DateTimeField(auto_now_add=True)
+    updated_at = models.DateTimeField(auto_now=True)
     #Foreign key
     #idUser = models.ForeignKey(User, on_delete=models.CASCADE)
 
@@ -17,6 +19,8 @@ class User(models.Model):
     avatar = models.TextField(null=True)
     enabled = models.BooleanField(default=True)
     id_wallet = models.ForeignKey(Wallet, on_delete=models.CASCADE)
+    created_at = models.DateTimeField(auto_now_add=True)
+    updated_at = models.DateTimeField(auto_now=True)
 
 class Server(models.Model):
     game = models.TextField()
@@ -25,3 +29,5 @@ class Server(models.Model):
     ram = models.DecimalField(decimal_places=2, max_digits=8)
     storage = models.DecimalField(decimal_places=2, max_digits=8)
     id_user = models.ForeignKey(User, on_delete=models.CASCADE)
+    created_at = models.DateTimeField(auto_now_add=True)
+    updated_at = models.DateTimeField(auto_now=True)
