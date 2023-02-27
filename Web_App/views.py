539c7751a338a400ce28b48b016b32e746b81673
Niuerex <kevincodcraft@gmail.com>
2023-02-27 17:14:44 +0100

    nose

--- Web_App/views.py
+++ Web_App/views.py
@@ -1,3 +1,10 @@
 from django.shortcuts import render
 
 # Create your views here.
+# https://www.geeksforgeeks.org/django-templates/
+def landing_page(request):
+    context = {
+        "data": "Gfg is the best",
+        "list": [1, 2, 3, 4, 5, 6, 7, 8, 9, 10]
+    }
+    return render(request, 'landingPage.html', context)
